0da259f644c8df0abe5ed574a771fd0748dfca50
Andrew Phommathep <andrew@starclubltd.com>
2015-10-06 09:36:19 -0700

    7 SKLabelNodes experiment

---
+++ SKLabelNodes_experiment/SKLabelNodes_experiment/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  SKLabelNodes_experiment
+//
+//  Created by Andrew Phommathep on 10/6/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let simpleLabel = SKLabelNode(fontNamed: "Copperplate")
+        simpleLabel.text = "Hello, Sprite Kit!"
+        simpleLabel.fontSize = 40
+        simpleLabel.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
+        addChild(simpleLabel)
+    }
+    
+//    override func didMoveToView(view: SKView) {
+//        /* Setup your scene here */
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!";
+//        myLabel.fontSize = 65;
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+//        
+//        self.addChild(myLabel)
+//    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
