15127b8201fa86d5faab476462e0711fb7c6a3be
Andrew Phommathep <andrew@starclubltd.com>
2015-10-07 09:55:52 -0700

    7 he simple SKLabelNode example with its verticalAlignmentMode set to SKLabelVerticalAlignmentMode.Center
    the text has shifted down so that the vertical center of the text is at
    the simpleLabel’s origin

--- SKLabelNodes_experiment/SKLabelNodes_experiment/GameScene.swift
+++ SKLabelNodes_experiment/SKLabelNodes_experiment/GameScene.swift
@@ -20,6 +20,7 @@ class GameScene: SKScene {
                     y: frame.height - simpleLabel.frame.height)
         
         simpleLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
+        simpleLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
         
         addChild(simpleLabel)
     }
