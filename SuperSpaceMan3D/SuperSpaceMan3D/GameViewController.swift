3200ba5df9376817738fb059db155d4d909c3bf2
Andrew Phommathep <andrew@starclubltd.com>
2015-10-12 12:17:57 -0700

    10 Init SuperSpaceMan3D

---
+++ SuperSpaceMan3D/SuperSpaceMan3D/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  SuperSpaceMan3D
+//
+//  Created by Andrew Phommathep on 10/12/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = SCNView()
+    }
+}
