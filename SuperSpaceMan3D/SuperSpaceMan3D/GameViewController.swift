3527caedcf7d502fb47f620659ae3e0ff43d4301
Andrew Phommathep <andrew@starclubltd.com>
2015-10-15 09:30:08 -0700

    10 Building the Scene
    Example does not run

--- SuperSpaceMan3D/SuperSpaceMan3D/GameViewController.swift
+++ SuperSpaceMan3D/SuperSpaceMan3D/GameViewController.swift
@@ -14,7 +14,36 @@ import SceneKit
 class GameViewController: UIViewController {
     
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-        self.view = SCNView()
+        // Create the scene
+        let mainScene = createMainScene()
+        mainScene.rootNode.addChildNode( createFloorNode() )
+
+        // Get the games main view
+        let sceneView = self.view as! SCNView
+        sceneView.scene = mainScene
+    
+        // Optional, but nice to be turned on during developement
+        sceneView.showsStatistics = true
+        sceneView.allowsCameraControl = true
+    }
+    
+    func createMainScene() -> SCNScene {
+        
+        var mainScene = SCNScene(named: "art.scnassets/hero.dae")
+        
+        return mainScene!
+    }
+    
+    
+    func createFloorNode() -> SCNNode {
+        
+        let floorNode = SCNNode()
+        let floor =  SCNFloor()
+            
+        floorNode.geometry = floor
+        floorNode.geometry?.firstMaterial?.diffuse.contents = "Floor"
+        return floorNode
     }
 }
